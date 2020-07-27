7e32d81f945b89ee7bcb5d5223104fc55993310f
Clara F <clarafok3@gmail.com>
2020-07-27 11:36:02 -0700

    Update MainFrame.java

--- ui/MainFrame.java
+++ ui/MainFrame.java
@@ -462,26 +462,4 @@ public class MainFrame extends JFrame {
 	}
 
 	
-	//method to download data from the database by joining tables through id's
-	public ResultSet downloadData(PurchaseDao purchaseDao) {
-		
-		ResultSet rs = null;
-		
-		try {
-			
-			String sql = "SELECT A00957354_Customers.customer_id, A00957354_Customers.firstName, A00957354_Customers.lastName, A00957354_Books.original_title, price "
-						+ "FROM ((A00957354_Purchases "
-						+ "INNER JOIN A00957354_Customers ON A00957354_Purchases.customer_id = A00957354_Customers.customer_id)"
-						+ "INNER JOIN A00957354_Books ON A00957354_Purchases.book_id = A00957354_Books.book_id)";
-			
-			rs = purchaseDao.makeQuery(sql);
-			
-			
-		} catch (ApplicationException e2) {
-			LOG.error("Error while making query to join three tables", e2);
-			JOptionPane.showMessageDialog(MainFrame.this, "Failed to retrieve purchase details", "Warning", 2);
-		}
-		return rs;
-	}
-	
 }
